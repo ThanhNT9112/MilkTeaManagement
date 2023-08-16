9cee23256a2662ce86b62c23c07358033cd82b37
ThanhNT9112 <thanhnthe160345@fpt.edu.vn>
2023-08-16 14:04:41 +0700

    commit

---
+++ MilkTea/Login/Models/Branch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Login.Models
+{
+    public partial class Branch
+    {
+        public Branch()
+        {
+            Orders = new HashSet<Order>();
+        }
+
+        public int BranchId { get; set; }
+        public string? BranchName { get; set; }
+        public int? ManagerId { get; set; }
+        public string? Phone { get; set; }
+        public string? Address { get; set; }
+
+        public virtual Manager? Manager { get; set; }
+        public virtual ICollection<Order> Orders { get; set; }
+    }
+}
