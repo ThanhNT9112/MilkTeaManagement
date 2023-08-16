c072f6f0a43b7e005ea5004c807866e5352f8973
ThanhNT9112 <thanhnthe160345@fpt.edu.vn>
2023-08-16 16:33:04 +0700

    commit

---
+++ MilkTea/MilkTea/Models/Account.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MilkTea.Models
+{
+    public partial class Account
+    {
+        public Account()
+        {
+            Managers = new HashSet<Manager>();
+        }
+
+        public int AccountId { get; set; }
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+        public int? RoleId { get; set; }
+        public int? BranchId { get; set; }
+
+        public virtual Branch? Branch { get; set; }
+        public virtual Role? Role { get; set; }
+        public virtual ICollection<Manager> Managers { get; set; }
+    }
+}
---
+++ MilkTea/MilkTea/Models/Manager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MilkTea.Models
+{
+    public partial class Manager
+    {
+        public int ManagerId { get; set; }
+        public int? AccountId { get; set; }
+        public string? Name { get; set; }
+        public bool? Gender { get; set; }
+        public DateTime? Dob { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+
+        public virtual Account? Account { get; set; }
+    }
+}
